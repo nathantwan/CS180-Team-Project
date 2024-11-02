2ca917b06ce168c9c18f4af7e2739376c0549986
nathantwan <136389134+nathantwan@users.noreply.github.com>
2024-11-02 13:19:18 -0400

    Update PostDatabase.java

--- PostDatabase.java
+++ PostDatabase.java
@@ -1,26 +1,49 @@
+import java.util.ArrayList;
+import javax.swing.ImageIcon;
 public class PostDatabase {
-    private ArrayList<Post> feed;
+    private static ArrayList<Post> feed = new ArrayList<>();
     public PostDatabase () {
-        feed = new ArrayList(Post)<>;
     }
     public void addPost(String caption, ImageIcon image, User user) {
         
         try {
-            Post post = new Post(caption, iamge, user);
+            Post post = new Post(caption, image, user);
             feed.add(post);
-        } catch {
+        } catch (InvalidPostException e){
             System.out.println("Invalid Post");
         }
     }
     public void editPost(Post post, String caption, ImageIcon image) {
+        int index = -1;
+        for (int i = 0; i < feed.size(); i++) {
+            if (feed.get(i).equals(post)) {
+                index = i;
+                break;
+            }
+        }
+        if (!(index == -1)) {
+            feed.get(index).setPost(caption, image);
+        } else {
+            System.out.println("Post does not exist");
+        }
 
         
     }
     public void deletePost(Post post) {
+        int index = -1;
+        for (int i = 0; i < feed.size(); i++) {
+            if (feed.get(i).equals(post)) {
+                index = i;
+                break;
+            }
+        }
+        if (!(index == -1)) {
+            feed.remove(index);
+        } else {
+            System.out.println("Post does not exist");
+        }
         
     }
 
-
-    
 
 }
