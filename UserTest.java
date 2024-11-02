3cea852797e23569587ac3facddd365477e6d4b9
Rafael Fletes <rfletes@purdue.edu>
2024-11-02 13:22:07 -0400

    Update UserTest.java

--- UserTest.java
+++ UserTest.java
@@ -1,3 +1,6 @@
+import org.junit.Before;
+import org.junit.Test;
+
 import javax.swing.*;
 import java.util.ArrayList;
 
@@ -14,7 +17,7 @@ public class UserTest {
     * Add more thorough case that access multiple methods at once if needed
      */
 
-    @org.junit.Before //Creates initialized variables that are used for all following tests
+    @Before //Creates initialized variables that are used for all following tests
     public void setUp() throws Exception {
         testImageIcon = new ImageIcon("s");
         testUser1 = new User("Sarah", "Leitzel", "SarahL", "password123", testImageIcon);
@@ -22,63 +25,63 @@ public class UserTest {
         testUser3 = new User("Eduardo", "Garcia", "EduardoG", "superCool14", testImageIcon);
     }
 
-    @org.junit.Test
+    @Test
     public void getPassword() {
         assertEquals("password123", testUser1.getPassword());
         assertEquals("IamCool45", testUser2.getPassword());
         assertEquals("superCool14", testUser3.getPassword());
     }
 
-    @org.junit.Test
+    @Test
     public void getUsername() {
         assertEquals("SarahL", testUser1.getUsername());
         assertEquals("WilliamS", testUser2.getUsername());
         assertEquals("EduardoG", testUser3.getUsername());
     }
 
-    @org.junit.Test
+    @Test
     public void getFirstName() {
         assertEquals("Sarah", testUser1.getFirstName());
         assertEquals("William", testUser2.getFirstName());
         assertEquals("Eduardo", testUser3.getFirstName());
     }
 
-    @org.junit.Test
+    @Test
     public void getLastName() {
         assertEquals("Leitzel", testUser1.getLastName());
         assertEquals("Smith", testUser2.getLastName());
         assertEquals("Garcia", testUser3.getLastName());
     }
 
-    @org.junit.Test
+    @Test
     public void getName() {
         assertEquals("Sarah Leitzel", testUser1.getName());
         assertEquals("William Smith", testUser2.getName());
         assertEquals("Eduardo Garcia", testUser3.getName());
     }
 
-    @org.junit.Test
+    @Test
     public void getProfilePicture() {
         assertEquals("s", testUser1.getProfilePicture().getDescription());
     }
 
-    @org.junit.Test
+    @Test
     public void getFriends() {
         assertEquals(0, testUser1.getFriends().size());
     }
 
-    @org.junit.Test
+    @Test
     public void getBlocked() {
         assertEquals(0, testUser1.getBlocked().size());
     }
 
-    @org.junit.Test
+    @Test
     public void setUsername() {
         testUser1.setUsername("NewSarah");
         assertEquals("NewSarah", testUser1.getUsername());
     }
 
-    @org.junit.Test
+    @Test
     public void setFriends() {
         ArrayList<User> newFriends = new ArrayList<>();
         newFriends.add(testUser2);
@@ -88,7 +91,7 @@ public class UserTest {
         assertEquals(testUser2, testUser1.getFriends().get(0));
     }
 
-    @org.junit.Test
+    @Test
     public void setBlocked() {
         ArrayList<User> blockedUsers = new ArrayList<>();
         blockedUsers.add(testUser2);
@@ -97,28 +100,28 @@ public class UserTest {
         assertEquals(testUser2, testUser1.getBlocked().get(0));
     }
 
-    @org.junit.Test
+    @Test
     public void setProfilePicture() {
         ImageIcon newProfilePic = new ImageIcon("newPath");
         testUser1.setProfilePicure(newProfilePic);
         assertEquals("newPath", testUser1.getProfilePicture().getDescription());
     }
 
-    @org.junit.Test
+    @Test
     public void testEquals() {
         User newUser = new User("Sarah", "Leitzel", "SarahL", "password123", testImageIcon);
         assertTrue(testUser1.equals(newUser));
         assertFalse(testUser1.equals(testUser2));
     }
 
-    @org.junit.Test
+    @Test
     public void setPassword() {
         assertTrue(testUser1.setPassword("password123", "password321"));
         assertEquals("password321", testUser1.getPassword());
         assertFalse(testUser1.setPassword("wrongPassword", "password321"));
     }
 
-    @org.junit.Test
+    @Test
     public void addFriend() {
         testUser1.addFriend(testUser2);
         assertEquals(1, testUser1.getFriends().size());
@@ -126,7 +129,7 @@ public class UserTest {
         assertTrue(testUser1.getFriends().contains(testUser2));
     }
 
-    @org.junit.Test
+    @Test
     public void blockUser() {
         //Add a section that adds a person to the friends list and assertsTrue
         testUser1.blockUser(testUser2);
@@ -136,21 +139,21 @@ public class UserTest {
         assertFalse(testUser1.getFriends().contains(testUser2));
     }
 
-    @org.junit.Test
+    @Test
     public void unblock() {
         testUser1.blockUser(testUser2);
         testUser1.unblock(testUser2);
         assertFalse(testUser1.getBlocked().contains(testUser2));
     }
 
-    @org.junit.Test
+    @Test
     public void removeFriend() {
         testUser1.addFriend(testUser2);
         testUser1.removeFriend(testUser2);
         assertFalse(testUser1.getFriends().contains(testUser2));
     }
 
-    @org.junit.Test
+    @Test
     public void testToString() {
         //Add a
 
