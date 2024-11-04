c6eccc0354a3e0d51390241cf62b75753b6ee498
nathantwan <136389134+nathantwan@users.noreply.github.com>
2024-11-03 22:47:59 -0500

    Update CommentInterface.java

--- CommentInterface.java
+++ CommentInterface.java
@@ -1,3 +1,11 @@
+/**
+ * Comment Interface
+ * <p>
+ * Purdue University -- CS18000 -- Team Project
+ *
+ * @author Yajushi
+ * @version Nov 3, 2024
+ */
 public interface CommentInterface {
     void setComment(String text);
     String getText();
